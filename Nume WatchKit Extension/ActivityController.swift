9fba41a8aee90156e5c8c44bc5379df0d773bc15
Daniel Hsu <danieljhsu@gmail.com>
2015-05-05 14:30:49 -0700

    Reordered the interface so that it rates numbers first, then prompts users for activity.

--- Nume WatchKit Extension/ActivityController.swift
+++ Nume WatchKit Extension/ActivityController.swift
@@ -1,60 +1,42 @@
 //
-//  InterfaceController.swift
-//  Nume WatchKit Extension
+//  RateNumberController.swift
+//  Nume
 //
-//  Created by Daniel Hsu on 4/29/15.
+//  Created by Daniel Hsu on 5/4/15.
 //  Copyright (c) 2015 Daniel Hsu. All rights reserved.
 //
 
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
-    override init() {
-        super.init()
-    }
+class ActivityController: WKInterfaceController {
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    
-    //create number ratings
-    @IBOutlet weak var ratingLabel: WKInterfaceLabel!
-    @IBOutlet weak var slider: WKInterfaceSlider!
-    
-    var ratingNumber = 0
-    @IBAction func updateRating(value: Float) {
-        ratingNumber = Int(value)
-        ratingLabel.setText("\(ratingNumber)")
 
-    }
-   
-    
     //create button image
     @IBOutlet weak var microphoneButton: WKInterfaceButton!
-    
     @IBOutlet weak var textResult: WKInterfaceLabel!
     @IBAction func onVoiceDictationTap() {
-        presentTextInputControllerWithSuggestions(nil, allowedInputMode: .Plain)
+        presentTextInputControllerWithSuggestions(["Hanging out", "Drinking coffee","Watching movie","Studying","Listening to music","Going for a run", "Shopping","Driving","Cooking"], allowedInputMode: .AllowEmoji)
             {(input) -> Void in
-                self.textResult.setText("\(input)")
+                self.textResult.setText(input[0] as? String)
                 println("INPUT: \(input)")
         }
         
     }
-
+    
 }
