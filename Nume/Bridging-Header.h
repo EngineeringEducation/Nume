2832934a338df6acd2464dfb4ec68b4ef6b14df0
engjasonp <engjasonp@gmail.com>
2015-05-05 16:40:00 -0700

    Merged my stuff with Daniel's stuff

---
+++ Nume/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Nume
+//
+//  Created by Jason Eng on 5/2/15.
+//  Copyright (c) 2015 Daniel Hsu. All rights reserved.
+//
+
+#ifndef Nume_Bridging_Header_h
+#define Nume_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
