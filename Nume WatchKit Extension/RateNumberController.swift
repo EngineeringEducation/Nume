0710d98c28866b7b0f6d71249ed808fae47da214
Daniel Hsu <danieljhsu@gmail.com>
2015-05-06 17:08:38 -0700

    Implemented new + and - buttons, linked to add and subtract between range of -5 and 5.

--- Nume WatchKit Extension/RateNumberController.swift
+++ Nume WatchKit Extension/RateNumberController.swift
@@ -27,16 +27,47 @@ class RateNumberController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    //create number ratings
-    @IBOutlet weak var ratingLabel: WKInterfaceLabel!
-    @IBOutlet weak var slider: WKInterfaceSlider!
     
+    //create plus and minus buttons for rating numbers
+    @IBOutlet weak var addNumber: WKInterfaceButton!
+    @IBOutlet weak var minusNumber: WKInterfaceButton!
+    @IBOutlet weak var ratingLabel: WKInterfaceLabel!
+
     var ratingNumber = 0
-    @IBAction func updateRating(value: Float) {
-        ratingNumber = Int(value)
-        ratingLabel.setText("\(ratingNumber)")
-        
+    @IBAction func increaseRating() {
+        minusNumber.setEnabled(true)
+        if ratingNumber == 4 {
+            ratingNumber += 1
+            ratingLabel.setText("\(ratingNumber)")
+            addNumber.setEnabled(false)
+        } else {
+            addNumber.setEnabled(true)
+            ratingNumber += 1
+            ratingLabel.setText("\(ratingNumber)")
+        }
     }
+    
+    @IBAction func decreaseRating() {
+        addNumber.setEnabled(true)
+        if ratingNumber == -4 {
+            ratingNumber -= 1
+            ratingLabel.setText("\(ratingNumber)")
+            minusNumber.setEnabled(false)
+        } else {
+            minusNumber.setEnabled(true)
+            ratingNumber -= 1
+            ratingLabel.setText("\(ratingNumber)")
+        }
+    }
+    
+    //create number ratings
+//    @IBOutlet weak var slider: WKInterfaceSlider!
+    
+//    @IBAction func updateRating(value: Float) {
+//        ratingNumber = Int(value)
+//        ratingLabel.setText("\(ratingNumber)")
+//        
+//    }
     
    
 
