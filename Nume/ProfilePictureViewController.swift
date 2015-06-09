596309b9e87678c741a17a063e16c6647a1739a8
engjasonp <engjasonp@gmail.com>
2015-06-08 17:32:23 -0700

    LoginView and ProfilePicView properly displayed.

--- Nume/ProfilePictureViewController.swift
+++ Nume/ProfilePictureViewController.swift
@@ -10,17 +10,57 @@ import UIKit
 
 class ProfilePictureViewController: UIViewController {
     
-    @IBOutlet weak var profilePic:FBSDKProfilePictureView?
-    
+    @IBOutlet weak var profilePic: FBSDKProfilePictureView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // if there is NO user token, then show the login screen.
+        // else, populate the profile picture
         
-        self.profilePic!.layer.cornerRadius = 75
-        self.profilePic!.clipsToBounds = true
-        self.profilePic!.layer.borderColor = UIColor.whiteColor().CGColor
-        self.profilePic!.layer.borderWidth = 3.0
-        
-        self.profilePic = FBSDKProfilePictureView()
-        println(self.profilePic)
+        if (FBSDKAccessToken.currentAccessToken() == nil) {
+            var login = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
+            self.presentViewController(login, animated: true, completion: nil)
+            
+        } else {
+            self.returnUserData()
+            self.profilePic!.layer.cornerRadius = 75
+            self.profilePic!.clipsToBounds = true
+            self.profilePic!.layer.borderColor = UIColor.whiteColor().CGColor
+            self.profilePic!.layer.borderWidth = 3.0
+
+            
+        }
+    }
+    
+    // method to grab the Users Facebook data. You can call this method anytime after a user has logged in by calling self.returnUserData()
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil) {
+                // Process error
+                println("Error: \(error)")
+            } else {
+                println("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                println("User Name is: \(userName)")
+                if let userEmail : NSString = result.valueForKey("email") as? NSString {
+                    println("User Email is: \(userEmail)")
+                }
+                
+                let appGroupID = "group.io.github.dhsu210.Nume"
+                if let defaults = NSUserDefaults(suiteName: appGroupID) {
+                    defaults.setValue(userName, forKey: "userNameKey")
+                }
+                
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                println("User Email is: \(userEmail)")
+                
+            }
+        })
     }
 }
